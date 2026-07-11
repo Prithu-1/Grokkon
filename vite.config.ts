945d47fc2779f5de33ea7d57cb28a9727d7247e7
Prithu Mishra <pchomulal@gmail.com>
2026-07-11 17:11:06 +0530

    Configure preview to open in new window

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,10 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    open: 'new-window',
+  },
+  preview: {
+    open: 'new-window',
+  },
 })
